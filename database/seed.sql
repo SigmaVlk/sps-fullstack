c6038dbc1e1648ac2578a214164bdef8ecfc39c6
Jaroslav Josef Černý <jaroslav.cerny@student.sps-cl.cz>
2025-06-07 15:24:45 +0200

    feat: minor changes

--- database/seed.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO items (name, description) VALUES 
-('Nakoupit mléko', 'Mléko pro dort'),
-('Vyprat rty', 'Rty jsou plné'),
-('Nakoupit chleb', 'Chleb pro dort'),
-('Nakoupit jídlo pro psa', 'Pepa potřebuje jídlo');
