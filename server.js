b2a7b320b971034dc7ae859cbe439a7d39f8b108
Jaroslav Josef Černý <jaroslav.cerny@student.sps-cl.cz>
2025-06-09 09:53:23 +0200

    feat: error 404 for wrong path

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const path = require('path');
 
 const partsRoutes = require('./api/parts');
 const suppliersRoutes = require('./api/suppliers');
@@ -12,6 +13,11 @@ app.use(express.static('public'));
 app.use('/api/parts', partsRoutes);
 app.use('/api/suppliers', suppliersRoutes);
 app.use('/api/orders', ordersRoutes);
+
+app.use((req, res, next) => {
+
+  res.status(404).sendFile(__dirname + '/public/404.html');
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
